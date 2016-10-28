92ddd0be5b73b30723651df18cb9dd516e5fcf07
dzucconi <mail@damonzucconi.com>
2016-10-28 13:31:19 +0200

    Refactor to re-use DOM nodes

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -1,60 +1,23 @@
+import Marquee from './lib/marquee';
+
 const DOM = {
   app: document.getElementById('app'),
 };
 
-const marquee = () => {
-  const el = document.createElement('DIV');
-  el.className = 'marquee';
-  DOM.app.appendChild(el);
-  return el;
-};
-
-const segment = (container, text) => {
-  const el = document.createElement('DIV');
-  el.className = 'crawler';
-  el.innerText = text;
-  container.appendChild(el);
-  return el;
-};
-
-const queue = (container, text, amount = 4) =>
-  new Promise(resolve => {
-    const el = segment(container, text + '\xa0');
-
-    const bounds = -(el.offsetWidth);
-    const limit = -(el.offsetWidth + container.offsetWidth);
-
-    let left = 0;
-
-    function tick () {
-      el.style.transform = `translateX(${left -= amount}px)`;
-
-      // Next
-      if (left < bounds) resolve();
-
-      // Animate
-      if (left > limit) window.requestAnimationFrame(tick);
-
-      // Done
-      if (left <= limit) container.removeChild(el);
-    }
-    tick();
-
-    return el;
-  });
-
 export default () => {
-  const run = (container, lines, amount = 4) =>
-    lines.reduce((promise, line) => {
-      return promise.then(() => queue(container, line, amount));
-    }, Promise.resolve(true))
-    .then(() => run(container, lines, amount));
+  const marquees = [
+    new Marquee('most boring', 7),
+    new Marquee('more boring', 6),
+    new Marquee('boring', 5),
+    new Marquee('normal', 4),
+    new Marquee('interesting', 3),
+    new Marquee('more interesting', 2),
+    new Marquee('most interesting', 1),
+  ];
 
-  run(marquee(), ['fastest'], 7);
-  run(marquee(), ['faster'], 6);
-  run(marquee(), ['fast'], 5);
-  run(marquee(), ['normal'], 4);
-  run(marquee(), ['slow'], 3);
-  run(marquee(), ['slower'], 2);
-  run(marquee(), ['slowest'], 1);
+  marquees
+    .map(m => {
+      DOM.app.appendChild(m.render(window.innerHeight / marquees.length));
+      m.run();
+    });
 };
---
+++ app/javascripts/lib/marquee.js
@@ -0,0 +1,60 @@
+export default class Marquee {
+  constructor(text, speed) {
+    this.text = text;
+    this.speed = speed || 1;
+    this.pool = [];
+  }
+
+  render(size) {
+    this.el = document.createElement('DIV');
+    this.el.className = 'marquee';
+
+    this.el.style.fontSize = `${size / 3}px`;
+    this.el.style.height = `${size}px`;
+    this.el.style.lineHeight = `${size}px`;
+
+    return this.el;
+  }
+
+  segment() {
+    const el = document.createElement('DIV');
+    el.className = 'crawler';
+    el.innerText = this.text + '\xa0';
+    this.el.appendChild(el);
+    return el;
+  }
+
+  queue() {
+    return new Promise(resolve => {
+      const el = this.pool.pop() || this.segment();
+
+      const bounds = -(el.offsetWidth);
+      const limit = -(el.offsetWidth + this.el.offsetWidth);
+
+      let left = 0;
+
+      const tick = () => {
+        el.style.transform = `translateX(${left -= this.speed}px)`;
+
+        // Next
+        if (left < bounds) resolve();
+
+        // Animate
+        if (left > limit) window.requestAnimationFrame(tick);
+
+        // Pool
+        if (left <= limit) this.pool.push(el);
+      };
+
+      tick();
+
+      return el;
+    });
+  }
+
+  run() {
+    this.queue()
+      .then(() => this.run());
+  }
+}
+
