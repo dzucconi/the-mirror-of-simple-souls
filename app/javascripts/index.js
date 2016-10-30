1c82f48bb0215689bbc84eb50954fd262073e95c
dzucconi <mail@damonzucconi.com>
2016-10-30 14:26:47 +0100

    Updates queryparams and refactor configuration

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -1,5 +1,4 @@
-import params from 'queryparams';
-import qs from 'qs';
+import parameters from 'queryparams';
 import debounce from 'debounce';
 import Marquee from './lib/marquee';
 import height from './lib/height';
@@ -8,31 +7,31 @@ const DOM = {
   app: document.getElementById('app'),
 };
 
-window.url = xs => qs.stringify(xs, { indices: false });
+window.parameters = parameters;
 
 export default () => {
-  const PARAMS = params();
-
-  const xs = Object.keys(PARAMS).map(key => PARAMS[key]);
-
-  const config = xs.length ? xs : [
-    ['fastest', 7],
-    ['faster', 6],
-    ['fast', 5],
-    ['normal', 4],
-    ['slow', 3],
-    ['slower', 2],
-    ['slowest', 1],
-  ];
+  const { text, ratio } = parameters({
+    ratio: 2,
+    text: [
+      ['fastest', 7],
+      ['faster', 6],
+      ['fast', 5],
+      ['normal', 4],
+      ['slow', 3],
+      ['slower', 2],
+      ['slowest', 1],
+    ]
+  });
 
   let marquees = [];
 
   const init = () => {
-    marquees = config.map(([text, speed]) => new Marquee(text, speed));
+    marquees = text.map(([message, speed]) =>
+      new Marquee(message, speed));
 
     marquees
       .map(m => {
-        DOM.app.appendChild(m.render(height(DOM.app) / marquees.length));
+        DOM.app.appendChild(m.render(height(DOM.app) / marquees.length, ratio));
         m.run();
       });
   };
